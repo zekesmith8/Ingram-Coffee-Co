3c6df4f8ffe00f8102cadd49da1de1a3bfeac66c
Zeke <zekesmith8@gmail.com>
2022-03-03 14:32:59 -0800

    making fixtures to start paving the way of the app

--- Ingram-Coffee/src/App.tsx
+++ Ingram-Coffee/src/App.tsx
@@ -7,38 +7,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-
       <div className='tester'>
       <div className="main__wrapper">
       <main>
@@ -76,6 +44,9 @@ function App() {
             itself as the best place to build, test, and discover front-end
             code, has declined to comment at this time.
           </p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
         </div>
         <a
           className="terrarium"
